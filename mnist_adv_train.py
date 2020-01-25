8bc90ca9c9daa19a75ba649a192533186911ec1e
AugustineCha <ac4612@columbia.edu>
2020-01-25 13:42:48 -0500

    Update mnist_adv_train.py

--- mnist_adv_train.py
+++ mnist_adv_train.py
@@ -13,7 +13,7 @@ from adversarialbox.attacks import FGSMAttack, LinfPGDAttack
 from adversarialbox.train import adv_train, FGSM_train_rnd
 from adversarialbox.utils import to_var, pred_batch, test
 
-from models import *
+from models import LeNet5, LeNet7
 
 
 # Hyper-parameters
